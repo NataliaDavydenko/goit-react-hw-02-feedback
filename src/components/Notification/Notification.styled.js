54feb3e3be8192485588a02227355ecbb26b6cc0
Natalia <davydenko.n@ukr.net>
2022-11-12 20:17:20 +0200

    done task

---
+++ src/components/Notification/Notification.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Text =styled.p`
+    color: red;
+    font-size: 25px;
+    font-weight:bold;
+`
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  color: black;
+  font-size: 30px;
+  padding-top: 10px;
+  padding-bottom: 15px;
+  margin-top: 30px;
+  margin-bottom: 15px;
+`;
